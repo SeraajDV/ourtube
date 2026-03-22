45b1392e4c5b4130443ee88a2ed15ef7708a2095
Seraaj de Villiers <seraajdevilliers@gmail.com>
2026-03-22 21:12:03 +0200

    feat: restructure theme components and add Navbar for improved layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const montserrat = Montserrat({ subsets: ["latin"], variable: "--font-sans" });
 
