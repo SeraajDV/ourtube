0b7904c5bac3cf9cb13e83fd79eb8d7b7ea29236
Seraaj de Villiers <seraajdevilliers@gmail.com>
2026-03-22 22:02:23 +0200

    feat: implement Navbar component with responsive design and integrated Sheet functionality

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,5 +1,39 @@
+import { Menu, Pause, PauseIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Sheet>
+      <nav className="flex items-center px-6 py-4 justify-between">
+        <div className="flex items-center gap-6">
+          <SheetTrigger>
+            <Menu size={22} />
+          </SheetTrigger>
+          <div className="flex items-center">
+            <div className="bg-primary px-2.5 py-1 rounded-sm mr-1">
+              <PauseIcon size={12} />
+            </div>
+            <h1 className="font-bold text-lg leading-tight tracking-tight">OurTube</h1>
+          </div>
+        </div>
+        <div className="flex-1 bg-amber-400">middle</div>
+        <div>
+          <p className="">lol</p>
+        </div>
+      </nav>
+      <SheetContent side="left" showCloseButton={false}>
+        <SheetHeader className="flex">
+          <SheetTitle>OurTube</SheetTitle>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 }
 
 export default Navbar;
